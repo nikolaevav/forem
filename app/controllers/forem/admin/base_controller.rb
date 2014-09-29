a44346cb0e7d2d719cdc62a449b517ecba7734c5
Igor Vaganov <i.vaganov@socialquantum.ru>
2014-09-29 14:35:52 +0400

    Fixes

--- app/controllers/forem/admin/base_controller.rb
+++ app/controllers/forem/admin/base_controller.rb
@@ -1,7 +1,7 @@
 module Forem
   module Admin
     class BaseController < ApplicationController
-      layout "forem/default"
+      layout Forem.layout
       
       before_filter :authenticate_forem_admin
 
